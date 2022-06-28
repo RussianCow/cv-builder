ad6d9bffea9f4ae0a24d3de553f2b8a5c8f9ea63
Sasha Chedygov <sasha@chedygov.com>
2022-06-27 18:36:00 -0700

    Rework builder to output to a separate out/ dir

--- builder.py
+++ builder.py
@@ -2,6 +2,8 @@
 
 from datetime import date
 import logging
+import os
+import shutil
 import subprocess
 import sys
 
@@ -13,22 +15,37 @@ import yaml
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+out_dir = 'out'
+public_dir = 'public'
+
 
 def build(data_file_name):
     """Loads the data in the file given by `data_file_name` and builds
        the CV."""
+    clean_output()
+    os.makedirs(out_dir, exist_ok=True)
     with open(data_file_name) as f:
         data = yaml.load(f, Loader=yaml.CLoader)
         data['last_updated'] = get_current_date()
         build_html(data)
     build_css(['style.less'])
+    copy_public_files()
     logger.info('Successfully built CV')
+
+
+def clean_output():
+    for root_path, dirs, files in os.walk(out_dir):
+        for d in dirs:
+            shutil.rmtree(os.path.join(root_path, d))
+        for f in files:
+            os.unlink(os.path.join(root_path, f))
 
 
 def build_html(data):
     """Renders templates with the data and writes the output HTML file."""
     rendered = render_template('index.html', **data)
-    with open('out.html', 'w') as out_file:
+    out_path = os.path.join(out_dir, 'index.html')
+    with open(out_path, 'w') as out_file:
         out_file.write(rendered)
 
 
@@ -37,9 +54,16 @@ def build_css(file_names):
        the same name."""
     for file_name in file_names:
         css_file_name = '.'.join(file_name.split('.')[:-1]) + '.css'
+        css_file_path = os.path.join(out_dir, css_file_name)
         which_ret = subprocess.run(['which', 'lessc'], capture_output=True)
         lessc_path = which_ret.stdout.rstrip(b'\n')
-        subprocess.run([lessc_path, file_name, css_file_name])
+        subprocess.run([lessc_path, file_name, css_file_path])
+
+
+def copy_public_files():
+    for root_path, dirs, files in os.walk(public_dir):
+        for f in files:
+            shutil.copyfile(os.path.join(root_path, f), os.path.join(out_dir, f))
 
 
 def render_template(file_name, *args, **kwargs):
@@ -63,8 +87,10 @@ def get_current_date():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+    )
     data_file_name = sys.argv[1]
     build(data_file_name)
--- watch.sh
+++ watch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Watching for changes..."
-watchmedo shell-command -c 'python builder.py data.yaml' -i './out.html;*.css'
+watchmedo shell-command -c 'python builder.py data.yaml' -i './out/'
