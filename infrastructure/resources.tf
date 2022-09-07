bd3282634d5824ab3681959049ac78765734613c
Sasha Chedygov <sasha@chedygov.com>
2022-09-06 17:15:27 -0700

    Add IPv6 domain records

--- infrastructure/resources.tf
+++ infrastructure/resources.tf
@@ -182,10 +182,34 @@ resource "aws_route53_record" "root-a" {
   }
 }
 
+resource "aws_route53_record" "root-aaaa" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name = local.domain_name
+  type = "AAAA"
+
+  alias {
+    name = aws_cloudfront_distribution.root.domain_name
+    zone_id = aws_cloudfront_distribution.root.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
 resource "aws_route53_record" "www-a" {
   zone_id = data.aws_route53_zone.main.zone_id
   name = "www.${local.domain_name}"
   type = "A"
+
+  alias {
+    name = aws_cloudfront_distribution.www.domain_name
+    zone_id = aws_cloudfront_distribution.www.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www-aaaa" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name = "www.${local.domain_name}"
+  type = "AAAA"
 
   alias {
     name = aws_cloudfront_distribution.www.domain_name
